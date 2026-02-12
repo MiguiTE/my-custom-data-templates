57799e7247f8414c72b9c1fa7e2670ef6045f9ab
MiguiTE! <miguite19@gmail.com>
2026-02-12 13:12:52 +0100

    gcp project agnostic

--- sftp-extractor/function/credentials/project_info.py
+++ sftp-extractor/function/credentials/project_info.py
@@ -2,15 +2,15 @@ import os
 from enum import Enum
 
 class PROJECT_INFO(Enum):
-	PROD_PROJECT_ID = "gc-p-plataformadeldato-ingesta"
+	PROD_PROJECT_ID = "PROD-PROJECT-ID"
 
-	PROJECT_ID = os.environ.get("PROJECTID", "gc-t-plataformadeldato-ingesta")
-	PROJECT_NUMBER = os.environ.get("PROJECTNUMBER", "455912236365")
+	PROJECT_ID = os.environ.get("PROJECTID", "TEST-PRROJECT-ID")
+	PROJECT_NUMBER = os.environ.get("PROJECTNUMBER", "TEST-PROJECT-NUMBER")
 
 	IS_DEV = PROD_PROJECT_ID != PROJECT_ID
 
 class PROCESS_INFO(Enum):
-	PROCESS_NAME = os.environ.get("PROCESSNAME", "rrhh").lower()
+	PROCESS_NAME = os.environ.get("PROCESSNAME", "PROCESS_NAME").lower()
 	SFTP_HOST = os.environ.get("SFTPHOST", "host")
 	SFTP_PORT = int(os.environ.get("SFTPPORT", "2222"))
 	SFTP_FOLDER = os.environ.get("SFTPFOLDER", "")
